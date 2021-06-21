0bb7958d6979d7410823aa1d3c457378263e4ad0
rowin <rowin@anhydride-acide.fr>
2021-06-22 00:28:55 +0200

    Creation

---
+++ splitfile.py
@@ -0,0 +1,57 @@
+import yaml
+from PyPDF2 import PdfFileWriter, PdfFileReader  # type: ignore
+
+
+def get_split_intervals(split_positions: list, length: int) -> list[list[int]]:
+    """Returns page intervals from position where to split
+
+    - split_positions: list of pages where to split. The split occurs BEFORE the page.
+        For example, split at page 3 on 5 pages document will give [1, 2], [3, 4, 5]
+    - length: length of the document, in pages
+    """
+    intervals = []
+    remaining_pages = list(range(1, length + 1))
+    for position in split_positions:
+        i = remaining_pages.index(position)
+        intervals.append(remaining_pages[:i])
+        remaining_pages = remaining_pages[i:]
+    intervals.append(remaining_pages)
+
+    return intervals
+
+
+def get_config(filename: str) -> dict:
+    with open(filename, "r") as config_file:
+        config = yaml.load(config_file)
+        return config["files"]
+
+
+def split_file(file_config: dict) -> None:
+    with open(file_config["name"], "rb") as infile:
+        inputpdf = PdfFileReader(infile)
+
+        split_intervals = get_split_intervals(file_config["split"], inputpdf.numPages)
+        for interval, outfilename in zip(split_intervals, file_config["sub_files"]):
+            export_page_range(inputpdf, interval, outfilename)
+
+
+def export_page_range(
+    inputpdf: PdfFileReader, interval: list[int], outfilename: str
+) -> None:
+    outputpdf = PdfFileWriter()
+    for page in interval:
+        outputpdf.addPage(inputpdf.getPage(page-1))
+
+    with open(outfilename, "wb") as outfile:
+        outputpdf.write(outfile)
+
+
+def main(config_filename: str) -> None:
+    files_config = get_config(config_filename)
+
+    for file_config in files_config:
+        split_file(file_config)
+
+
+if __name__ == "__main__":
+    main('config.yaml')
---
+++ test_splitfile.py
@@ -0,0 +1,22 @@
+import PyPDF2
+from splitfile import get_split_intervals, export_page_range
+from unittest import mock
+
+def test_get_split_interval_various_index():
+    assert get_split_intervals([], 10) == [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+    assert get_split_intervals([2], 10) == [[1], [2, 3, 4, 5, 6, 7, 8, 9, 10]]
+    assert get_split_intervals([2, 4], 10) == [[1], [2, 3], [4, 5, 6, 7, 8, 9, 10]]
+    assert get_split_intervals([2, 10], 10) == [[1], [2, 3, 4, 5, 6, 7, 8, 9], [10]]
+
+
+
+def test_export_page_range(mocker):
+    inputpdf = mocker.MagicMock()
+    inputpdf.getPage = mocker.MagicMock()
+    mocker.patch("PyPDF2.pdf.PdfFileWriter")
+    mocker.patch.object(PyPDF2.PdfFileWriter, 'addPage')
+    m = mocker.mock_open()
+    export_page_range(inputpdf, [1, 2], "out.pdf")
+
+    inputpdf.getPage.assert_any_call(1)
+    inputpdf.getPage.assert_any_call(2)
